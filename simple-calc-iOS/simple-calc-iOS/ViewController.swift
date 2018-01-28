9ea3b5f3ed286f0ff62503a2ede879897f5205fe
Ruihe (Jerry) Li <ruiheli@uw.edu>
2018-01-27 23:19:22 -0800

    UI complete

--- simple-calc-iOS/simple-calc-iOS/ViewController.swift
+++ simple-calc-iOS/simple-calc-iOS/ViewController.swift
@@ -10,6 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ioTextField: UITextField!
+    
+    @IBOutlet weak var btnOne: UIButton!
+    @IBOutlet weak var btnTwo: UIButton!
+    @IBOutlet weak var btnThree: UIButton!
+    @IBOutlet weak var btnFour: UIButton!
+    @IBOutlet weak var btnFive: UIButton!
+    @IBOutlet weak var btnSix: UIButton!
+    @IBOutlet weak var btnSeven: UIButton!
+    @IBOutlet weak var btnEight: UIButton!
+    @IBOutlet weak var btnNine: UIButton!
+    @IBOutlet weak var btnZero: UIButton!
+    @IBOutlet weak var btnPlus: UIButton!
+    @IBOutlet weak var btnMinus: UIButton!
+    @IBOutlet weak var btnMultiply: UIButton!
+    @IBOutlet weak var btnDivide: UIButton!
+    @IBOutlet weak var btnCount: UIButton!
+    @IBOutlet weak var btnAvg: UIButton!
+    @IBOutlet weak var btnFact: UIButton!
+    @IBOutlet weak var btnEquals: UIButton!
+    @IBOutlet weak var btnClear: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +42,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func prventErrorOp(_ clicked: UIButton){
+        if(btnAvg != clicked){
+            btnAvg.isEnabled = false
+        }
+        if(btnCount != clicked){
+            btnCount.isEnabled = false
+        }
+        
+        btnFact.isEnabled = false
+    }
 
+    @IBAction func handleClear(_ sender: UIButton) {
+        btnCount.isEnabled = true
+        btnAvg.isEnabled = true
+        btnFact.isEnabled = true
+        ioTextField.text = ""
+    }
+    
 }
 
