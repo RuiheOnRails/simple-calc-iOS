2bffca0857f98076b21b2848258a718b980971ba
Ruihe (Jerry) Li <ruiheli@uw.edu>
2018-01-27 22:31:47 -0800

    project set up

---
+++ simple-calc-iOS/simple-calc-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simple-calc-iOS
+//
+//  Created by Jerry Li on 1/27/18.
+//  Copyright © 2018 Jerry Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
